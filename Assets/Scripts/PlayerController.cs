6830f78f4ebc9131135998e115cf7d44723e34dd
Tapio Kursula <tkursula@gmail.com>
2017-11-16 09:15:00 +0200

    Added player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 2f;
+    //public float jumpSensitivity;
+    public float mouseSensitivity = 5f;
+
+    CharacterController player;
+    public GameObject eyes;
+
+    float movementFB;
+    float movementLR;
+
+    float rotX;
+    float rotY;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        player = GetComponent<CharacterController>();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Player movement
+        movementFB = Input.GetAxis("Vertical") * speed;
+        movementLR = Input.GetAxis("Horizontal") * speed;
+
+        rotX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        rotY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        Vector3 movement = new Vector3(movementFB, 0, -movementLR);
+        movement = transform.rotation * movement;
+        player.Move(movement * Time.deltaTime);
+
+
+        transform.Rotate(0, rotX, 0);
+        eyes.transform.Rotate(-rotY, 0, 0);
+        //Mouse movement
+
+
+
+
+
+    }
+}
