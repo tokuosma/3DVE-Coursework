0c3e93eb552efcdcfa29afb554b66b94f57b1889
Toni Kuosmanen <toni.t.kuosmanen@gmail.com>
2017-11-27 23:12:06 +0200

    Added witch from drive. Added navmesh to city hall. NavAgent test

---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,22 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
+
+public class MoveTo : MonoBehaviour {
+
+	public Transform goal;
+	private NavMeshAgent agent;
+    
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		agent.SetDestination (goal.transform.position);
+		
+	}	
+
+	void Update(){
+        agent.SetDestination(goal.transform.position);
+
+    }
+
+}
