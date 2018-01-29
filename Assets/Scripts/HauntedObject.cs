4fdb3bfe52ab5dc439712eed46bc872f9c54e498
Toni Kuosmanen <toni.t.kuosmanen@gmail.com>
2018-01-29 18:43:13 +0200

    Added spooky whispering to haunted objects.

--- Assets/Scripts/HauntedObject.cs
+++ Assets/Scripts/HauntedObject.cs
@@ -6,6 +6,10 @@ public class HauntedObject : MonoBehaviour {
 
     public bool isScanned;
     private static float emissionIncrement = 0.03f;
+
+    public AudioClip scannedSound;
+    public AudioClip ghostlyWhisper;
+
 	// Use this for initialization
 	void Start () {
         isScanned = false;
@@ -26,6 +30,9 @@ public class HauntedObject : MonoBehaviour {
     private IEnumerator ObjectScanned()
     {
         var renderer = GetComponent<Renderer>();
+        var audioSource = GetComponent<AudioSource>();
+        audioSource.Stop();
+        audioSource.PlayOneShot(scannedSound);
         Material mat = renderer.material;
         Color baseColor = new Color(0f, 0.9586205f, 1f, 1f); //Replace this with whatever you want for your base color at emission level '1'
 
